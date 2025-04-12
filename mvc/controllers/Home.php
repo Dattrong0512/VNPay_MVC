3da699875ca428fea8055936c75310d2e460ece1
Dat Trong <trongleviet05@gmail.com>
2025-04-12 21:33:24 +0700

    first commit, create model mvc basic

---
+++ mvc/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+// http://localhost/live/Home/Show/1/2
+
+require_once("./mvc/core/Controller.php");
+class Home {
+
+    // Must have SayHi()
+    function SayHi(){
+      echo "Dit me may";
+
+    }
+
+}
+?>
