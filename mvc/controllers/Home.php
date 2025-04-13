ea4f41e374509c5c611919d95765ae78bc17805b
Dat Trong <trongleviet05@gmail.com>
2025-04-13 20:43:06 +0700

    Add some class

--- mvc/controllers/Home.php
+++ mvc/controllers/Home.php
@@ -3,13 +3,16 @@
 // http://localhost/live/Home/Show/1/2
 
 require_once("./mvc/core/Controller.php");
-class Home {
+class Home extends Controller
+{
 
-    // Must have SayHi()
-    function SayHi(){
-      echo "Dit me may";
-
-    }
-
+  // Must have SayHi()
+  function SayHi()
+  {
+    echo "Dit me may";
+  }
+  function Show()
+  {
+    $this->view("Home");
+  }
 }
-?>
